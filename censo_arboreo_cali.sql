31c796c5a77ea3bdc944ddea2b976df1b60f13d2
Geovanny <birdcode.py@gmail.com>
2024-02-25 18:52:51 -0500

    Creation of table of number of trees by species

--- censo_arboreo_cali.sql
+++ censo_arboreo_cali.sql
@@ -1,4 +1,6 @@
 CREATE EXTENSION postgis;
 
-SELECT nombre_cie AS scientific_name, COUNT(DISTINCT id) AS number_of_trees
-FROM censo_arboreo_cali GROUP BY scientific_name ORDER BY number_of_trees DESC;
+SELECT nombre_cie AS scientific_name, nombre_com AS nombre_comun_es,
+COUNT(DISTINCT id) AS number_of_trees FROM censo_arboreo_cali 
+GROUP BY scientific_name, nombre_comun_es 
+ORDER BY number_of_trees DESC;
